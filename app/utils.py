d2825330714a3e54e2a1f625b9ffde372cbb5402
mlincon <mehdi.lincon@gmail.com>
2020-08-02 18:28:35 +0200

    adjust indentation

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,11 @@
 def credentials_to_dict(credentials):
     cred_dict = {
         'token': credentials.token,
-            'refresh_token': credentials.refresh_token,
-            'token_uri': credentials.token_uri,
-            'client_id': credentials.client_id,
-            'client_secret': credentials.client_secret,
-            'scopes': credentials.scopes
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes
     }
 
     return cred_dict
