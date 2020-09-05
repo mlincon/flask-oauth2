64af0b9a4f361475b0a0d5a769178f320b1650a2
mlincon <mehdi.lincon@gmail.com>
2020-09-05 23:00:56 +0200

    include flask migration

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,27 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import MetaData
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+
+
+# SQLite3 apparently does not support ALTER tables, hence provide a naming convention
+# use batch rendering option
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+db = SQLAlchemy(app, metadata=MetaData(naming_convention=convention))
+if db.engine.url.drivername == 'sqlite':
+    migrate = Migrate(app, db, render_as_batch=True)
+else:
+    migrate = Migrate(app, db)
+
 
 from app import routes, login, models
