ed8d59792f97ed530a2198e8cf775c6c4aab7ffe
mlincon <mehdi.lincon@gmail.com>
2020-09-05 22:59:42 +0200

    include user table

--- app/models.py
+++ app/models.py
@@ -1,10 +1,29 @@
 from app import db
 
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250))
+    picture = db.Column(db.String(250))
+
+    def __repr__(self):
+       return {
+           'id'      : self.id,
+           'name'    : self.name,
+           'email'   : self.email,
+           'picture' : self.picture
+       }
+
+
 class Restaurant(db.Model):
     __tablename__ = 'restaurant' 
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     # the __repr__ method tells Python how to print objects of this class
     def __repr__(self):
@@ -24,6 +43,7 @@ class MenuItem(db.Model):
     course = db.Column(db.String(250))
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
     restaurant = db.relationship('Restaurant')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
        return {
